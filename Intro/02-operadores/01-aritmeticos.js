4a015331afd1e1f53623b671a63a07e985cc0811
Elias <mayorelias83@gmail.com>
2024-05-10 10:59:44 -0600

    Clase 10 operadores aritmeticos

---
+++ Intro/02-operadores/01-aritmeticos.js
@@ -0,0 +1,26 @@
+let a = 5;
+
+let b = 3;
+
+let suma = a + b; // adicion 
+console.log(suma); // 8
+
+let resta = a - b; // sustraccion
+console.log(resta); // 2
+
+let multiplicacion = a * b; // multiplicacion
+console.log(multiplicacion); // 15
+
+let division = a / b; // division
+console.log(division); // 1.6666666666666667
+
+let modulo = a % b; // modulo
+console.log(modulo); // 2
+
+let incremento = a++; // incremento
+console.log(incremento); // 5
+
+let decremento = a--; // decremento
+console.log(decremento); // 6
+
+
