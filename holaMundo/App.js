2a4d9e9fd323bde0322a805450ed43446c79fbee
Elias <mayorelias83@gmail.com>
2024-06-19 15:15:17 -0600

    Avances hasta practica 9

--- holaMundo/App.js
+++ holaMundo/App.js
@@ -1,28 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native'; /* Importaciones de componentes */
+import { Button, StyleSheet, Text, View, TextInput, TouchableHighlight, TouchableOpacity, TouchableWithoutFeedback } from 'react-native'; /* Importaciones de componentes */
 import React, {useState} from 'react'; /* Importaciones de librerias */
 
-const Texto =({estilo})=>{
-  const [contenido, setContenido] = useState('Hola tú');
-  const cambiarTexto = () => {
-    setContenido('Hola a todos');
-  } 
-  return (<Text style={[styles.text, estilo]} onPress={cambiarTexto}>{contenido}</Text>);
-} 
+// const Texto =({estilo})=>{
+//   const [contenido, setContenido] = useState('Hola tú');
+//   const cambiarTexto = () => {
+//     setContenido('Hola a todos');
+//   } 
+//   return (<Text style={[styles.text, estilo]} onPress={cambiarTexto}>{contenido}</Text>);
+// } 
 
 export default function App() {
+  const [text, setText] = useState('Texto Default'); 
+  const [submit, setSubmit] = useState(''); 
+
+  const [count, setCount] = useState(0);
+
+  const onPress = () => {
+    setCount(count + 1);
+  };
+
   return (
     <View style={styles.container}>
-      
-      <Texto estilo={styles.red}></Texto>
 
-      <Texto estilo={styles.green}></Texto>
+      <Text style={{color: "white",}} >Componente TextInput: {submit}</Text>
+      <TextInput style={styles.input} placeholder='Soy un input :)' onChangeText={(t)=>setText(t)}/>
 
-      <Texto estilo={styles.blue}></Texto>
-      
-      {/* <Button onPress={Texto.cambiarTexto} title="Presioname" color="#212121"></Button>  */}
-      <StatusBar style="auto" />   
+      <Button title='Presioname' style={{height: 40,}} onPress={()=>{setSubmit(text); alert('Alerta pendiente\n Alerta\n Alerta')}}></Button>
 
+      <View style={styles.countContainer}>
+        <Text style={styles.countText}>Count: {count}</Text>
+      </View>
+      <TouchableWithoutFeedback onPress={onPress}>
+        <View style={styles.button}>
+          <Text>Touch Here</Text>
+        </View>
+      </TouchableWithoutFeedback>
+
+
+      <StatusBar style="auto" />
     </View>
   );
 }
@@ -31,29 +47,52 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: '#fff',
+    backgroundColor: '#212121',
     alignItems: 'center',
     justifyContent: 'center',
   },
 
-  text: {
-    color: 'white',
-    fontSize: 25,
-  },
+    input: {
+      height: 40,
+      margin: 12,
+      borderWidth: 3,
+      backgroundColor: '#212121',
+      borderColor: 'white',
+      color: "white",
+      textAlign: 'center',
+    },
 
-  red: {
-    flex: 2,
-    backgroundColor: 'red',
-  },
+    button: {
+      alignItems: 'center',
+      backgroundColor: '#DDDDDD',
+      padding: 10,
+    },
+    countContainer: {
+      alignItems: 'center',
+      padding: 10,
+    },
+    countText: {
+      color: '#FF00FF',
+    },
 
-  green: {
-    flex: 2,
-    backgroundColor: 'green',
-  },
+  // text: {
+  //   color: 'white',
+  //   fontSize: 25,
+  // },
+
+  // red: {
+  //   flex: 2,
+  //   backgroundColor: 'red',
+  // },
+
+  // green: {
+  //   flex: 2,
+  //   backgroundColor: 'green',
+  // },
   
-  blue: {
-    flex: 2,
-    backgroundColor: 'blue',
-  }
+  // blue: {
+  //   flex: 2,
+  //   backgroundColor: 'blue',
+  // }
 
 });
---
+++ my-app/App.js
@@ -0,0 +1,70 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, Button, TextInput, View } from 'react-native';
+import React, {useState} from 'react';
+
+export default function App() {
+  const [text1, setText1] = useState('Texto Default'); 
+  const [text2, setText2] = useState('Texto Default'); 
+  const [text3, setText3] = useState('Texto Default'); 
+
+  const alerta = () =>{
+    alert('Nombre: '+text1+'\nEmail: '+text2+'\nPassword: '+text3);
+  };
+
+  return (
+    <View style={styles.container}>
+
+      <View style={{flex: 2}}/>
+
+      <View style={styles.card}>
+        <Text style={styles.texto}>Nombre:</Text>
+        <TextInput style={styles.input} inputMode='text' onChangeText={(t)=>setText1(t)}/>
+
+        <Text style={styles.texto}>Email:</Text>
+        <TextInput style={styles.input} inputMode='email' onChangeText={(t)=>setText2(t)}/>
+
+        <Text style={styles.texto}>Password:</Text>
+        <TextInput style={styles.input} onChangeText={(t)=>setText3(t)} secureTextEntry/>
+
+        <Button color='red' title="Guardar" onPress={() => {alerta();}}/>
+      </View>
+
+      <View style={{flex: 2}}></View>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: '#212121',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  card: {
+    flex: 3,
+    backgroundColor: 'white',
+    width: '90%',
+    padding: 20,
+    borderRadius: 10,
+    justifyContent: 'space-between',
+  },
+  texto: {
+    color: 'green',
+    fontSize: 18,
+  },
+  input: {
+    borderRadius: 5,
+    height: 40,
+    borderWidth: 3,
+    backgroundColor: 'lightgrey',
+    borderColor: 'lightgrey',
+  },
+
+
+
+  
+});
